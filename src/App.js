ca4584d83247e08d3c6bf97b65d1f976da30c0ea
Jose Bonito <joseantoniobonito@hotmail.com>
2020-03-22 11:24:56 -0300

    hello world!

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import "./App.css";
+
+const TABLE_LENGTH = 18;
+const COLORS = [
+  "orange",
+  "green",
+  "purple",
+];
+
+const getColorbyId = id => (id >= COLORS.length) ? "disabled" : COLORS[id];
+
+function useColors() {
+  const emptyArray = new Array(TABLE_LENGTH).fill(9);
+  const [colors, setColors] = useState(emptyArray)
+
+  const generateColors = () => {
+    setColors(emptyArray);
+    setTimeout(() =>
+      setColors(emptyArray.map(() => Math.floor(Math.random() * COLORS.length))
+      ), Math.random() * 3000
+    )
+  }
+
+  return [colors, generateColors];
+}
+
+function TodoList() {
+  const [colors, generateColors] = useColors();
+
+  return (
+    <div>
+      <div className={"table"}>
+        {colors.map((value, index) =>
+          <div key={index} className={getColorbyId(value)} />
+        )}
+        <input type="button" value="Generar" onClick={generateColors} />
+      </div>
+    </div>
+  )
+}
+
+export default TodoList;
